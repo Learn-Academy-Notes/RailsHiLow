23507b76d5bc01b6e1c9cef22b6119b712e8b920
learn <learn@learn01.localdomain>
2018-04-24 16:22:36 -0700

    password checker updated

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -9,9 +9,21 @@ class PasswordController < ApplicationController
 
         if @password.length < 6
             flash.now[:notice] = "Password must be greater than six characters."
+
+        elsif !@password.match(/^(?=.*[A-Z])./)
+            flash.now[:notice] = "Password must include at least one capital case character."
+        # elsif !@password.include? "A..Z"
+        #     flash.now[:notice] = "Password must include at least one capital case character."
+        elsif !@password.match(/^(?=.*[a-z])./)
+            flash.now[:notice] = "Password must include at least one lower case character."
+        # elsif !@password.include? "a..z"
+        #     flash.now[:notice] = "Password must include at least one lower case character."
+        elsif !@password.match(/^(?=.*[0-9])./)
+            flash.now[:notice] = "Password must include at least one numeric character."
+        # elsif !@password.include? "0..9"
+        #     flash.now[:notice] = "Password must include at least one numeric character."
         else
             cookies[:password] = @password
-
 
         end
     end
